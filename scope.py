23714c883944e9c6d5bb58e5ae09650b218d3a32
Zhao, Shaojun(shaojun) <shaojun@ebay.com>
2019-11-30 22:41:09 -0800

    minor

--- scope.py
+++ scope.py
@@ -2,16 +2,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# +
 fig, axes = plt.subplots(2, 1)
-
 x = np.random.randn(10000)
 _ = axes[0].hist(x, bins=100)
 y = np.random.rand(10000)
 _ = axes[1].hist(y, bins=100)
-# -
-
 
 print("hello world!")
-
-
+print('ok')
